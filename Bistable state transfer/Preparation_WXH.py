d293c02e473b0c545069ecf181d4e7665ef2a3b8
hanxiaowang <732760767@qq.com>
2024-12-27 16:33:05 +0800

    Changing the produce of initial path in Preparation_WXH.py

--- Bistable state transfer/Preparation_WXH.py
+++ Bistable state transfer/Preparation_WXH.py
@@ -23,11 +23,12 @@ class date_and_time():
 class save_file(date_and_time):
     def __init__(self):
         super().__init__()
-        path = f'C:\\Users\\AORUS\\OneDrive\\桌面\\bistable date'
-        if os.path.exists(path) == True:
-           pass
-        else:
-            os.mkdir(path)
+        ## Storing data with the file name containing the date when i used to measure.
+        # path = f'C:\\Users\\AORUS\\OneDrive\\桌面\\bistable date'
+        # if os.path.exists(path) == True:
+        #    pass
+        # else:
+        #     os.mkdir(path)
 
     def creat_initial_file(self, path: str):
         if os.path.exists(path) == True:
